1c281b342a3a32391f8b1a368bf26f3ffa9bbaab
Ajit <ajit.7its@gmail.com>
2025-10-27 16:13:40 +0530

    Edited NEXT JS Config File

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
      // ✅ Ignore ESLint errors during builds (useful for deployment)
     ignoreDuringBuilds: true,
+    eslint: {
+    ignoreDuringBuilds: true,
+  },
 }
 
 module.exports = nextConfig
