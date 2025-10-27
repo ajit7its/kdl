24309df78ebfaba80f9a1b467c6b5b567e02d772
Ajit <ajit.7its@gmail.com>
2025-10-27 16:38:21 +0530

    Edited Next Config

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   eslint: {
-    // ✅ Disable ESLint only when deploying on Vercel
+    // ✅ Disable ESLint only during Vercel builds
     ignoreDuringBuilds: isVercel,
   },
 };
